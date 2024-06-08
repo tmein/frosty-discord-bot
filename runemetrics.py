81795713ce9b78592e0e9771c3991325eb748c3c
tmein <ertfishycus@gmail.com>
2024-06-08 11:19:09 +1000

    more robust logging and handling for  missing runemetrics profiles

--- runemetrics.py
+++ runemetrics.py
@@ -12,22 +12,26 @@ type EventLogEntry = dict[str, str]
 def get_event_log(rsn: str) -> list[EventLogEntry]:
     response = requests.get(f'https://apps.runescape.com/runemetrics/profile/profile?user={rsn}&activities=20',
                             headers={'content-type': 'application/json'})
-    return response.json().get("activities")
+    activities = response.json().get("activities")
+    return activities if activities else []
 
 
 def extract_relevant_activities(player: Player, last_entry: LastEntry) -> list[Drop]:
     drops: list[Drop] = []
-    event_log = get_event_log(player.rsn)
-    for activity in event_log:
-        if last_entry.date and last_entry.date == activity["date"]:
-            break
-        elif "I found a" in activity["text"]:
-            utc_date = datetime.datetime.strptime(activity["date"], DATETIME_FORMAT)
-            utc_date = pytz.timezone('Europe/London').localize(utc_date).astimezone(pytz.utc)
-            drops.append(
-                Drop(player_id=player.player_id, player=player, message=activity["text"], date=utc_date))
-    if not event_log or len(event_log) == 0:
-        print(f"No activity found for {player.rsn}")
+    if player.rsn.startswith("*"):
+        return []
     else:
-        last_entry.date = event_log[0]["date"]
-    return drops
+        event_log = get_event_log(player.rsn)
+        for activity in event_log:
+            if last_entry.date and last_entry.date == activity["date"]:
+                break
+            elif "I found a" in activity["text"]:
+                utc_date = datetime.datetime.strptime(activity["date"], DATETIME_FORMAT)
+                utc_date = pytz.timezone('Europe/London').localize(utc_date).astimezone(pytz.utc)
+                drops.append(
+                    Drop(player_id=player.player_id, player=player, message=activity["text"], date=utc_date))
+        if not event_log or len(event_log) == 0:
+            print(f"No activity found for {player.rsn}")
+        else:
+            last_entry.date = event_log[0]["date"]
+        return drops
